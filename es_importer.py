7dad03773ea7c6a954e126c1234dd52406e0ca59
Neo Hu <neo.hu@dji.com>
2017-06-28 15:30:48 +0800

    使用join和es bulk加快导入速度

--- es_importer.py
+++ es_importer.py
@@ -6,7 +6,8 @@
 import psycopg2
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
-import datetime
+import os
+import time
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -64,6 +65,8 @@ join mfg_report_detail as mrd on (mr.id = mrd.report_id)"%(id_min, limit, offset
         cursor.execute(sql)
         join_items = cursor.fetchall()
         colnames = colnames or [desc[0] for desc in cursor.description]
+        if len(join_items) == 0:
+            break
         for val in join_items:
             report_id = val[0]
             report_info = batch.setdefault(report_id, {})
@@ -82,7 +85,6 @@ join mfg_report_detail as mrd on (mr.id = mrd.report_id)"%(id_min, limit, offset
                 detail[detail_col_name] = val[col_no]
             report_info.setdefault("details", []).append(detail)
         offset += len(batch)
-
         actions = []
         for key, val in batch.iteritems():
             action ={
@@ -93,43 +95,9 @@ join mfg_report_detail as mrd on (mr.id = mrd.report_id)"%(id_min, limit, offset
             }
             actions.append(action)
         helpers.bulk(es, actions)
-        # for mr_id, mr_doc in batch.iteritems():
-        #     es.index("dtas", "mr", mr_doc, mr_id)
-
-        # sql_mr = "select * from mfg_report where id > %d order by id limit 100 offset %s"%(id_min, offset)
-        # cur_mr.execute(sql_mr)
-        # mr_infos = cur_mr.fetchall()
-        # offset += len(mr_infos)
-        # for mr_item in mr_infos.itervalues():
-        #     mr_dict = {}
-        #     mr_colnames = mr_colnames or [desc[0] for desc in cur_mr.description]
-        #     for idx, colname in enumerate(mr_colnames):
-        #         val = mr_item[idx]
-        #         if val or val == 0:
-        #             if colname == "test_time":
-        #                 mr_dict[colname] = str(val)
-        #             elif colname == "test_date":
-        #                 mr_dict[colname] = val.strftime("%Y-%m-%d %H:%M:%S%Z")
-        #             else:
-        #                 mr_dict[colname] = val
-        #     mr_id = mr_dict["id"]
-        #     sql_mrd = "select * from mfg_report_detail as mrd " \
-        #               "where mrd.report_id = %d"%(mr_id,)
-        #     cur_mrd.execute(sql_mrd)
-        #     mrd_infos = cur_mrd.fetchall()
-        #     if mrd_infos:
-        #         mrd_colnames = mrd_colnames or [desc[0] for desc in cur_mrd.description]
-        #         for mrd_item in mrd_infos:
-        #             mrd_dict = {}
-        #             for idx , colname in enumerate(mrd_colnames):
-        #                 mrd_val = mrd_item[idx]
-        #                 if mrd_val or mrd_val == 0:
-        #                     mrd_dict[colname] = mrd_val
-        #             if mrd_dict:
-        #                 mr_dict.setdefault("details", []).append(mrd_dict)
-        #     # print offset, mr_id
-        #     # print(mr_dict["test_date"])
-        #     (es.index("dtas", "mr", mr_dict, mr_id))
-        #     # break
-        # else:
-        #     break
+        if sys.platform == "win32":
+            os.system("cls")
+        else:
+            os.system("clear")
+        print "...import date to %s, id = %s"%(join_items[-1][9], join_items[-1][0])
+        time.sleep(0.01)
