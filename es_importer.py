0d9907e80a0c20c5153ff83d784a0f5c50a69cdb
Neo Hu <neo.hu@dji.com>
2017-06-30 16:29:01 +0800

    自定义mapping

--- es_importer.py
+++ es_importer.py
@@ -1,7 +1,8 @@
+#-*- coding:utf-8 -*-
+
 # pip install -U pip
 # pip install psycopg2
 
-#-*- coding:utf-8 -*-
 
 import psycopg2
 from elasticsearch import Elasticsearch
@@ -19,22 +20,102 @@ es = Elasticsearch(hosts=[{"host": "localhost", "port": 9200}])
 
 def initmapping():
     global es
-    mapping = {
+    mappings = {
         "properties": {
             "test_date": {
                 "type": "date",
                 "format": "yyyy-MM-dd HH:mm:ssZZ"
             },
+            "station_id": {
+                "type": "string",
+                "index": "not_analyzed",
+            },
+            "station_name": {
+                "type": "string",
+                "index": "not_analyzed",
+            },
+            "dut_sn":{
+                "type": "string",
+                "analyzer": "tri_analyzer",
+            },
+            "test_time":{
+                "type": "string",
+                "index": "not_analyzed",
+            },
+            "test_site":{
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "op_id":{
+                "type": "string",
+                "index": "not_analyzed",
+            },
+            "part_number":{
+                "type": "string",
+                "index": "not_analyzed",
+            },
+            "test_sw_rev":{
+                "type": "string",
+                "index": "not_analyzed",
+            },
+            "pc_name": {
+                "type": "string",
+                "index": "not_analyzed",
+            },
+            # test_status为Pass, PASS, PAss...等，所以用默认的standard tokenizer转换为小写，查询时也要用小写的pass或者fail
+            # "test_status":{
+            #     "index": "standard"
+            # },
+            "log":{
+                "type": "text",
+                "index": "not_analyzed"
+            },
             "details":{
                 "type": "nested",
+                "properties":{
+                    "description": {
+                        "type": "string",
+                        "index": "not_analyzed",
+                    },
+                    "item_name":{
+                        "type": "string",
+                        "index": "not_analyzed",
+                    },
+                    "unit":{
+                        "type": "string",
+                        "index": "not_analyzed",
+                    },
+                    "detail":{
+                        "type": "text",
+                        "index": "not_analyzed",
+                    }
+                }
+            }
+        }
+    }
+    settings = {
+        "settings":{
+            "analysis": {
+                "analyzer": {
+                    "tri_analyzer": {
+                        "tokenizer": "tri_tokenizer"
+                    }
+                },
+                "tokenizer": {
+                    "tri_tokenizer": {
+                        "type": "ngram",
+                        "min_gram": 3,
+                        "max_gram": 14,
+                    }
+                }
             }
         }
     }
     try:
-        es.indices.create("dtas")
-        es.indices.put_mapping("mr", mapping, ["dtas"])
-    except:
-        pass
+        es.indices.create("dtas", settings)
+        es.indices.put_mapping("mr", mappings, ["dtas"])
+    except BaseException,e:
+        print str(e)
 
 def getStartId():
     body = {
