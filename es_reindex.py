37b7c484e1f8138c21cc28f1801d7a364b7cf460
Neo Hu <neo.hu@dji.com>
2017-12-14 12:26:13 +0800

    job-dtas 创建

--- es_reindex.py
+++ es_reindex.py
@@ -6,17 +6,14 @@ import sys
 
 es = Elasticsearch(hosts=[{"host": "localhost", "port": 9200}])
 
-def reinex(start, end):
-    index_name = "dtas-" + start[0:7]
-    body = {"source": {
-        "index": "dtas",
-        "type": "mr",
-        "query": {
-            "range": {"test_date": {"gte": start, "lt": end}}
-        }
+def reinex():
+    body = {
+        "source": {
+            "index": "dtas-201712",
+            "type": "mr",
     },
         "dest": {
-            "index": index_name,
+            "index": "dtas-201712_update",
             "type": "mr",
         }}
 
@@ -24,7 +21,4 @@ def reinex(start, end):
 
 # 2017-01-01 00:00:00+08 2017-02-01 00:00:00+08
 if __name__ == "__main__":
-    start = sys.argv[1]
-    end   = sys.argv[2]
-    print start, end
-    reinex(start, end)
+    reinex()
